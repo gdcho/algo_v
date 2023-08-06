af0d1321601857ed77a153d9e2aa0b1ca8104de0
David Cho <gcho13@my.bcit.ca>
2023-08-05 22:38:11 -0400

    [Add] Components for body and toolbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 "use client";
 import React from 'react';
 import Sorting from './components/Sorting/Sorting'; 
+import Toolbar from './components/Toolbar/Toolbar';
+import Body from './components/Body/Body';
 import { Provider } from 'react-redux';
 import { initializeStore } from './redux/store'; 
 
@@ -10,11 +12,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
         <div className="App">
           <Provider store={store}> 
+            <Toolbar />
+            <Body />
             <Sorting />
           </Provider>
         </div>
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,23 +1,26 @@
 // app/pages/index.tsx
 "use client";
-import React from 'react';
-import { Provider } from 'react-redux';
-import { initializeStore } from '../redux/store';
-import dynamic from 'next/dynamic';
-import { selectSortAlgorithm } from '../redux/actions';
+import React from "react";
+import { Provider } from "react-redux";
+import { initializeStore } from "../redux/store";
+import dynamic from "next/dynamic";
+import { selectSortAlgorithm } from "../redux/actions";
+import Toolbar from "../components/Toolbar/Toolbar";
+import Body from "../components/Body/Body";
 
-const DynamicSorting = dynamic(
-  () => import('../components/Sorting/Sorting'),
-  { ssr: false }
-)
+const DynamicSorting = dynamic(() => import("../components/Sorting/Sorting"), {
+  ssr: false,
+});
 
-const Home = ({initialReduxState}) => {
+const Home = ({ initialReduxState }) => {
   const store = initializeStore(initialReduxState);
 
   return (
     <div>
       <h1>Welcome to Algo Visualizer</h1>
       <Provider store={store}>
+        <Toolbar />
+        <Body />
         <DynamicSorting />
       </Provider>
     </div>
@@ -26,11 +29,10 @@ const Home = ({initialReduxState}) => {
 
 export async function getServerSideProps() {
   const reduxStore = initializeStore({});
-  
-  reduxStore.dispatch(selectSortAlgorithm('bubbleSort'));
-  
+
+  reduxStore.dispatch(selectSortAlgorithm("bubbleSort"));
+
   return { props: { initialReduxState: reduxStore.getState() } };
 }
 
 export default Home;
-
