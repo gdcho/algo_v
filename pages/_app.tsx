8098345856e7d2b64e28615d4bd5a734a9c65a53
gdcho <gcho13@my.bcit.ca>
2025-05-13 22:31:45 -0700

    update dependencies and add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,37 @@
-import '../styles/globals.css';
-import { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
-import store from '../app/redux/store';
-import Head from 'next/head';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../app/redux/store";
+import Head from "next/head";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      if (typeof window.gtag === "function") {
+        window.gtag("event", "page_view", {
+          page_path: url,
+          page_location: window.location.href,
+          page_title: document.title,
+        });
+      }
+    };
+
+    handleRouteChange(router.asPath);
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
   return (
     <Provider store={store}>
       <Head>
@@ -12,9 +39,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="Algorithm Visualizer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-HG87ZP376K"
+        strategy="afterInteractive"
+      />
+
+      {/* GA4 bootstrap */}
+      <Script id="ga-setup" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HG87ZP376K', { send_page_view: false });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </Provider>
   );
 }
-
-export default MyApp;
